0bbb78a6e8177ca610faae95d6e86e8b82fcb41f
Priyanshu Chaturvedi <messageforpriyanshu@gmail.com>
2023-12-24 11:51:57 +0530

    file-upload

--- server/cloudinary.js
+++ server/cloudinary.js
@@ -1,9 +1,9 @@
 const cloudinary = require("cloudinary").v2;
-          
+        
 cloudinary.config({ 
-  cloud_name: 'drjubgbgl', 
-  api_key: '244562595337494', 
-  api_secret: 'H4A2elX_Kdf2GxPWpn7OzIbvWFc' 
+  cloud_name: process.env.cloud_name, 
+  api_key: process.env.cloud_api_key, 
+  api_secret: process.env.cloud_api_secret 
 });
 
 module.exports = cloudinary;
